9b9ab06ea7d05a261821a4f9dd48d2e1b4f74ca1
MateusBU <mateusurtassum@gmail.com>
2025-09-14 19:31:34 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+export default {
+	name: "App"
+}
+</script>
+
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Bem-vindo ao Cadastro de ONGs</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: Home,
+}
+</script>
+
+<style>
+
+</style>
